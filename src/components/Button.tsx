6da5c6f64cc7db0d51ec3a3e7203eb962284f3c2
Pas0quit0 <marcos.marcos.arthur291@gmail.com>
2021-02-23 18:07:41 -0300

    commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+interface ButtonProps {
+    color: string;
+    children: string;
+}
+
+export function Button(props: ButtonProps) {
+    const [count, setCount] = useState(1)
+
+    const increment = () =>{
+        setCount(count+1);
+    }
+
+    return(
+        <button 
+            type="button" 
+            style={{backgroundColor: props.color}}
+            onClick={increment}
+        >
+            {props.children} <strong>{count}</strong>
+        </button>
+    )
+}
